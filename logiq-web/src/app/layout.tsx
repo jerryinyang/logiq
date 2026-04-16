24d1b46fa5b2e02e2c58dfd5a5a26441a5a17bcc
Prince <Prynzwill147@gmail.com>
2026-04-16 09:06:20 +0100

    NextJS project setup

---
+++ logiq-web/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Logiq",
+  description: "Modern gamified learning platform for everyone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${dmSans.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
